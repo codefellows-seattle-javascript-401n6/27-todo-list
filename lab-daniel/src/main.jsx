c56b8c5afe1dcae4f319e2fd73d1f8e69693b10c
Daniel Pillay <dpillay03@gmail.com>
2018-04-28 11:58:37 -0700

    landing page is now functional and renduring on page

--- lab-daniel/src/main.jsx
+++ lab-daniel/src/main.jsx
@@ -7,19 +7,27 @@ import {
     Link
 } from 'react-router-dom';
 
+import Landing from './components/landing.jsx'
+
 class App extends React.Component {
     constructor(props){
     super(props);
-    console.log('here');
     this.state = {
-        header: 'To-do list with React'
+        header: 'Execute - Track Your Goals!'
         }
     }
       
     render(){
         return <Router>
             <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Landing</Link></li>
+                        <li><Link to="/dashboard">Dashboard</Link></li>
+                    </ul>
+                </nav>
                 <h1>{this.state.header}</h1>
+                <Route exact path="/" component={Landing} />
             </div>
         </Router>
         
